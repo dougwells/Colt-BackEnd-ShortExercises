073a03c38e73fb0c8f8dd5159ca0e1896f14e615
Doug Wells <dougwells@gmail.com>
2016-03-04 12:53:15 -0700

    ejs JS logic & control in .ejs file

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static('public'));
+
 app.get("/", function(req, res){
     res.render("home.ejs")
 });
@@ -8,10 +10,18 @@ app.get("/", function(req, res){
 app.get("/fallinlovewith/:thing", function(req, res){
   var love =  req.params.thing;
   res.render("love.ejs", {
-      love: love, 
-      surfingURL: "http://surfcabarete.com/wp-content/uploads/2013/02/duckdive.jpeg",
-      skiingURL: "http://lorrainehuber.com/wp-content/uploads/2009/01/lori-skiing-sepp-mallaun.jpg"
+    love:love
   });
+});
+
+app.get("/posts", function(req, res){
+    var postsOut = [
+        {title:"Dogs are awesome", author:"Linda"},
+        {title:"Learn to drive a stick", author:"Sierra"},
+        {title:"Breaking the 6 minute mile", author: "Savannah"}
+        ]
+        
+        res.render("posts.ejs", {postsIn:postsOut});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
