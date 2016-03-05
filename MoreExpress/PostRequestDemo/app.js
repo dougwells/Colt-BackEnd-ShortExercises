cf180e61b107d3f5be32b2d597e84770687d37b3
Doug Wells <dougwells@gmail.com>
2016-03-05 09:11:12 -0700

    Post commands, push to array and re-render page w/updated data

---
+++ MoreExpress/PostRequestDemo/app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var friends = ["Ted", "Julian", "Paul"];
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render('home');
+});
+
+app.post("/addfriend", function(req, res){
+    friends.push(req.body.newfriend);
+    res.redirect('/friends')
+});
+
+app.get("/friends", function(req, res){
+
+    res.render('friends', {friends:friends});
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("All systems GO");
+});
