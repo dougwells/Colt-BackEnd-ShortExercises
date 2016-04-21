9d406787a4ddd1d3cdd79c9ec10db0362a74cead
Doug Wells <dougwells@gmail.com>
2016-04-21 08:32:22 -0600

    Data Associations.  Initial commit

--- Associations/embed.js
+++ Associations/embed.js
@@ -1,3 +1,35 @@
 var mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost/blog_demo");
+
+//User Collection: email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String
+});
+var User = mongoose.model('User', userSchema);
+
+//Post Collection: title, content
+
+var postSchema = new mongoose.Schema({
+   title: String,
+   content: String
+});
+
+var Post = mongoose.model("Post", postSchema);
+
+// var newUser = User.create({email: "bob@yahoo.com", name: "Bob"}, function(err, user){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(user);
+//     }
+// });
+
+var newPost = Post.create({title: "My first post", content:"Coding is fun"}, function(err, post){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(post);
+    }
+});
